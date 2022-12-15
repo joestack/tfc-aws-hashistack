5608e5c87868550ec20363afb82773f9c7b5c51d
joestack <joern@joern-online.de>
2022-12-15 08:49:18 +0100

    update on outputs 2

--- outputs.tf
+++ outputs.tf
@@ -1,8 +1,8 @@
-output "vcn_server_private_ips" {
+output "cluster_private_ips" {
   value = aws_instance.server.*.private_ip
 }
 
-output "vcn_server_public_ips" {
+output "cluster_public_ips" {
   value = aws_instance.server[*].public_ip
 }
 
@@ -14,14 +14,6 @@ output "nomad_client_public_ips" {
   value = aws_instance.client[*].public_ip
 }
 
-# output "fqdn_tls" {
-#   value = local.fqdn_tls
-# }
-
-# output "fqdn_tls_consul" {
-#   value = local.consul_fqdn_tls
-# }
-
 output "tfe_pub_ip" {
   value = aws_instance.tfe[*].public_ip
 }
@@ -30,14 +22,14 @@ output "tfe_prv_ip" {
   value = aws_instance.tfe[*].private_ip
 }
 
-output "server_count" {
+output "cluster_server_count" {
   value = local.server_count
 }
 
-output "tfe_enc_password_2" {
+output "tfe_enc_password" {
   value = var.terraform_enabled ? local.tfe_enc_password : ""
 }
 
-output "tfe_auth_passord_2" {
+output "tfe_auth_passord" {
   value = var.terraform_enabled ? local.tfe_auth_password : ""
 }

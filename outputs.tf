9643c0d92937c21934905c63a5995d0efd53a751
JoeStack <joern@hashicorp.com>
2024-01-08 15:26:33 +0100

    add consul_ca_file to outputs 1.1

--- outputs.tf
+++ outputs.tf
@@ -63,5 +63,5 @@ output "hashistack_subnet" {
 }
 
 output "consul_ca_file" {
-  value = base64_encode(tls_private_key.ca.*.public_key_pem[0])
+  value = base64encode(tls_private_key.ca.*.public_key_pem[0])
 }
